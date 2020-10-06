59ac0f3becc55e3c440f769e4490ca962cb7b11e
Amanda Yonce <amanda.stork@gmail.com>
2020-10-05 22:09:55 -0400

    profile page format

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./header"
+import { CardBody, Card, Col, Row, CardText } from 'reactstrap';
 
 
 class Profile extends Component {
@@ -8,6 +9,17 @@ class Profile extends Component {
     <div >
         <Header/>
         <h1>Profile</h1>
+        <Row style={{width: "60em", margin: "5em"}}>
+          <Col>
+            <Card style={{width: "25em", margin: "2em"}}>
+              <CardBody>
+                <CardText>User Profile</CardText>
+                <CardText>Username:</CardText>
+              </CardBody>
+            </Card>
+        </Col>
+       
+        </Row>
     </div>
   );
 }
