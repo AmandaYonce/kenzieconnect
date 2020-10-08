74bc154b5460b184dd89d5a1678865d4aadff207
Amanda Yonce <amanda.stork@gmail.com>
2020-10-07 20:16:42 -0400

    landing page styling

--- frontend/src/components/landingpage.js
+++ frontend/src/components/landingpage.js
@@ -1,37 +1,68 @@
 import React from "react";
 import Header from "./header";
 import { CardBody, Card, Col, Row, CardText } from "reactstrap";
+import placeholder from '../images/placeholder.png'
 
 const Landing = (props) => {
   return (
-    <React.Fragment>
+    <React.Fragment >
+      <div style={{backgroundColor: "#888888", height: "1000px"}}>
       <Header />
-      <Row style={{ width: "400px", margin: "5em" }}>
-        <a href="/messagefeed/:username">You have 4 New Messages</a>
+
+      <Row style={{ margin: '1em' }}>
+        <Card id="notifCard">
+          <a class="notifText" href="/messagefeed/:username">You have 4 New Messages</a>
+        </Card>
       </Row>
-      <Row style={{ width: "60em", margin: "5em" }}>
-        <Col>
-          <Card style={{ width: "25em", margin: "2em" }}>
+
+{/* Once we have data coming in we will delete all but 1 of the col/cards below and use map
+to display 3 random matches from the list of potentials */}
+      <Row style={{ justifyContent: "center" }} row-12>
+          <h1 style={{fontFamily: 'Montserrat', fontSize: "2.5em"}}>Your Daily Matches</h1>
+      </Row>
+      <Row style={{ margin: "2em" }} row-12>
+        
+        <Col col-3>
+          <Card style={{ width: "20em", margin: "0 auto"}}>
             <CardBody>
-              <CardText>Matched Profile</CardText>
+              <img alt="prfoilepic" src={placeholder} style={{width: '15em'}}/>
+              <CardText>Name:</CardText>
+              <CardText>Age:</CardText>
+              <CardText>Gender:</CardText>
+              <CardText>Sexual Preference:</CardText>
+              <CardText>Percentage Match:</CardText>
+              <CardText>Bio:</CardText>
             </CardBody>
           </Card>
         </Col>
-        <Col>
-          <Card style={{ width: "25em", margin: "2em" }}>
+        <Col col-3>
+          <Card style={{ width: "20em", margin: "0 auto"}}>
             <CardBody>
-              <CardText>Matched Profile</CardText>
+            <img alt="prfoilepic" src={placeholder} style={{width: '15em'}}/>
+              <CardText>Name:</CardText>
+              <CardText>Age:</CardText>
+              <CardText>Gender:</CardText>
+              <CardText>Sexual Preference:</CardText>
+              <CardText>Percentage Match:</CardText>
+              <CardText>Bio:</CardText>
             </CardBody>
           </Card>
         </Col>
-        <Col>
-          <Card style={{ width: "25em", margin: "2em" }}>
+        <Col col-3>
+          <Card style={{ width: "20em", margin: "0 auto" }}>
             <CardBody>
-              <CardText>Matched Profile</CardText>
+            <img alt="prfoilepic" src={placeholder} style={{width: '15em'}}/>
+              <CardText>Name:</CardText>
+              <CardText>Age:</CardText>
+              <CardText>Gender:</CardText>
+              <CardText>Sexual Preference:</CardText>
+              <CardText>Percentage Match:</CardText>
+              <CardText>Bio:</CardText>
             </CardBody>
           </Card>
         </Col>
       </Row>
+      </div>
     </React.Fragment>
   );
 };
