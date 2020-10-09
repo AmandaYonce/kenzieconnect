79ed22afa7e4b56ef6d08c2259c95416313b95b9
Amanda Yonce <amanda.stork@gmail.com>
2020-10-08 20:46:24 -0400

    created profile tips

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -2,22 +2,44 @@ import React from "react";
 import Header from "./header";
 import { CardBody, Card, Col, Row, CardText } from "reactstrap";
 import { StateContext } from "../App";
+import placeholder from "../images/placeholder.png";
+import Tips from "./tips";
 
 const Profile = (props) => {
   const { state } = React.useContext(StateContext);
   return (
-    <div>
+      <div style={{ backgroundColor: "#4a5066", height: "800px" }}>
       <Header />
-      <h1>Profile</h1>
+      <h1 class="profile">Your Profile</h1>
       <Row style={{ width: "60em", margin: "5em" }}>
         <Col>
-          <Card style={{ width: "25em", margin: "2em" }}>
-            <CardBody>
-              <CardText>User Profile</CardText>
-              <CardText>Username:{state.user}</CardText>
-            </CardBody>
-          </Card>
+          <Card
+        style={{
+          width: "25em",
+          margin: "0 auto",
+          boxShadow: "5px 10px 2px #888888",
+        }}
+      >
+        <CardBody>
+          <Row style={{ textAlign: "center" }}>
+            <img
+              alt="profilepic"
+              src={placeholder}
+              style={{ width: "10em", paddingRight: "1em", fontFamily: "Montserrat", }}
+            />
+          </Row>
+          <CardText>Name:{state.user}</CardText>
+          <CardText>Age:</CardText>
+          <CardText>Gender:</CardText>
+          <CardText>Sexual Preference:</CardText>
+          <CardText>Bio:</CardText>
+        </CardBody>
+      </Card>
+
         </Col>
+        <Col>
+        <Tips/>
+          </Col>
       </Row>
     </div>
   );
---
+++ frontend/src/components/tips.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { CardBody, Card, Col, Row, CardText } from "reactstrap";
+
+
+const Tips = (props) => {
+
+    return (
+        <>
+        <Card>
+            <CardBody>
+                <CardText>Profile Helpful Hints</CardText>
+            </CardBody>
+        </Card>
+        </>
+
+    )
+}
+export default Tips
