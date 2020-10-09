1c35387edf5e2183955bf13683ded73827fa1916
Amanda Yonce <amanda.stork@gmail.com>
2020-10-08 21:20:43 -0400

    profile page styling

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./header";
-import { CardBody, Card, Col, Row, CardText } from "reactstrap";
+import { CardBody, Card, Col, Row, CardText, Button } from "reactstrap";
 import { StateContext } from "../App";
 import placeholder from "../images/placeholder.png";
 import Tips from "./tips";
@@ -8,15 +8,15 @@ import Tips from "./tips";
 const Profile = (props) => {
   const { state } = React.useContext(StateContext);
   return (
-      <div style={{ backgroundColor: "#4a5066", height: "1000px" }}>
+      <div style={{ backgroundColor: "#4a5066", height: "1000px"}}>
       <Header />
       <h1 class="profile">Your Profile</h1>
-      <Row style={{ width: "60em", margin: "5em" }}>
-        <Col>
+      <div></div>
+      <Row style={{ width: "60em", marginTop: "2.5em", marginLeft: "auto", marginRight: "auto"}}>
+        <Col col-6 >
           <Card
         style={{
           width: "25em",
-          margin: "0 auto",
           boxShadow: "5px 10px 2px #888888",
         }}
       >
@@ -35,9 +35,11 @@ const Profile = (props) => {
           <CardText>Bio:</CardText>
         </CardBody>
       </Card>
-
+        {/* I don't know how we exactly we want to handle this but I definitely 
+        think it would be nice if we had some way to for the user to edit their profile */}
+          <Button size="lg" style={{marginTop: "2em"}}>Edit Profile</Button>
         </Col>
-        <Col>
+        <Col col-6>
         <Tips/>
           </Col>
       </Row>
