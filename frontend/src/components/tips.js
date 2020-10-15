ab1d980417b4f757e56f838ce42d226e0c8d978d
Kyle Negley <kpnegley@gmail.com>
2020-10-15 00:37:41 -0400

    refactored tips

--- frontend/src/components/tips.js
+++ frontend/src/components/tips.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { CardBody, Card, CardText } from "reactstrap";
-import check from "../images/check.png"
-
+import check from "../images/check.png";
 
 const Tips = (props) => {
+  const tips = [
+    "Don't Leave Anything Blank.",
+    "Be honest. People LOVE it.",
+    "Don’t write a novel.",
+    "Feel free to talk about your kids.",
+    "Add details.",
+    "Don’t talk about dating.",
+    "Get a little romantic.",
+    "Be your wonderful, weird self.",
+  ].map((value,index) => (
+    <CardText key={index}>
+      <img className="check" src={check} alt="check" />
+      {value}
+    </CardText>
+  ));
 
-    return (
-        <>
-        <Card>
-            <CardBody>
-                <CardText style={{fontFamily: 'Dosis', fontSize: "3em"}}>Profile Hints & Tips</CardText>
-                <div style={{fontFamily: 'Dosis', fontSize: "1.5em"}}>
-                <CardText><img className="check" src={check} alt="check"/>Don't Leave Anything Blank.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Be honest. People LOVE it.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Don’t hide your face in your profile photo.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Don’t write a novel.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Feel free to talk about your kids. </CardText>
-                <CardText><img className="check" src={check} alt="check"/>Add details.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Don’t talk about dating.</CardText>
-                <CardText><img className="check" src={check} alt="check"/>Get a little romantic. </CardText>
-                <CardText><img className="check" src={check} alt="check"/>Be your wonderful, weird self. </CardText>
-                </div>
-            </CardBody>
-        </Card>
-        </>
-
-    )
-}
-export default Tips
+  return (
+    <React.Fragment>
+      <Card>
+        <CardBody>
+          <CardText style={{ fontFamily: "Dosis", fontSize: "3em" }}>
+            Profile Hints & Tips
+          </CardText>
+          <div style={{ fontFamily: "Dosis", fontSize: "1.5em" }}>{tips}</div>
+        </CardBody>
+      </Card>
+    </React.Fragment>
+  );
+};
+export default Tips;
